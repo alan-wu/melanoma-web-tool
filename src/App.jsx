8fc024bf4c006efd8addb7424a18c3d1ede144d9
Arnav Dogra <adog951@aucklanduni.ac.nz>
2026-03-16 09:41:38 +1300

    feat: update routing to parent and child layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar.jsx";
 import FloatingLogo from "./components/FloatingLogo.jsx";
 
 import Home from "./pages/Home.jsx";
+import ToolsLayout from "./pages/ToolsLayout.jsx";
 import Tool1 from "./pages/Tool1.jsx";
 import Tool2 from "./pages/Tool2.jsx";
 import Team from "./pages/Team.jsx";
@@ -18,13 +19,15 @@ export default function App() {
       <Box component="main" sx={{ flex: 1, minHeight: 0, width: "100%" }}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/tool1" element={<Tool1 />} />
-          <Route path="/tool2" element={<Tool2 />} />
+          <Route element={<ToolsLayout />}>
+            <Route path="/tool1" element={<Tool1 />} />
+            <Route path="/tool2" element={<Tool2 />} />
+          </Route>
           <Route path="/team" element={<Team />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-        <FloatingLogo src={`${import.meta.env.BASE_URL}images/abilogo.png`}/>
-           
+        <FloatingLogo src={`${import.meta.env.BASE_URL}images/abilogo.png`} />
+
       </Box>
     </Box>
   );
