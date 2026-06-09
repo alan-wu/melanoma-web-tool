afd2b71e2536cf7669aea4563c303c08693323c2
alan-wu <alan.wu@auckland.ac.nz>
2026-06-09 15:26:43 +1200

    Fix deploygment issue.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 
 export default defineConfig({
-  base: '/Melanoma-web-tool/',
+  base: '/melanoma-web-tool/',
+  plugins: [react()]
 });
 
